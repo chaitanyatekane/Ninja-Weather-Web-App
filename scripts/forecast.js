cdf96dc5ce4144282943bc2b0a25977733291e48
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-03-18 08:59:53 +0530

    Used OOP Concepts

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,29 +1,36 @@
-const key = 'h8Na5z4m4YBvG6hMDyRN5cHZwQAHSY1n';
+class Forecast {
+    constructor(){
+        this.key = 'h8Na5z4m4YBvG6hMDyRN5cHZwQAHSY1n';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';        
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+
+        return { cityDets, weather };
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
 
 
+// const key = 'h8Na5z4m4YBvG6hMDyRN5cHZwQAHSY1n';
 // get weather information
-const getWeather = async (id) => {
-
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
-
-
+// const getWeather = async (id) => {
+// };
 // get city information
-const getCity = async (city) => {
-
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
+// const getCity = async (city) => { 
+// };
 
  
